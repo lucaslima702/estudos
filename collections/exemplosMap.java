74e3544b5b92eedee57f385d0cf806c6f0ddfa82
lucaslima702 <limxlucas@icloud.com>
2024-01-05 06:38:15 -0300

    05/01/2024

--- collections/exemplosMap.java
+++ collections/exemplosMap.java
@@ -6,11 +6,13 @@ import java.util.Map;
 public class exemplosMap {
 
 	public static void main(String[] args) {
+		//UTILIZADO PARA MAPEAR SEUS OBJETOS COMO SE FOSSE UM SUPER ID
 		Map<Long, Pessoa> mapa = new HashMap<>();
 		Pessoa lucas = new Pessoa(1L, "Lucas");
 		
 		mapa.put(1L, lucas);
 		
 		Pessoa lucas2 = mapa.get(1L);
+		System.out.println(lucas2.getNome() + " " + lucas.getId());
 	}
 }
