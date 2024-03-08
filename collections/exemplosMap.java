a2469e0dca33ba706628629e358c16367fa66d6d
lucaslima702 <limxlucas@icloud.com>
2024-03-08 02:12:46 -0300

    08/03

--- collections/exemplosMap.java
+++ collections/exemplosMap.java
@@ -1,26 +1,18 @@
 package br.com.collections;
 
-import java.net.URL;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class exemplosMap {
 
 	public static void main(String[] args) {
 		//UTILIZADO PARA MAPEAR SEUS OBJETOS COMO SE FOSSE UM SUPER ID
-//		Map<Long, Pessoa> mapa = new HashMap<>();
-//		Pessoa lucas = new Pessoa(1L, "Lucas");
-//		
-//		mapa.put(1L, lucas);
-//		
-//		Pessoa lucas2 = mapa.get(1L);
-//		System.out.println(lucas2.getNome() + " " + lucas.getId());
+		Map<Long, Pessoa> mapa = new HashMap<>();
+		Pessoa lucas = new Pessoa(1L, "Lucas");
 		
-		Scanner teclado = new Scanner(System.in);
-		String nomeDoFilmeEscrito = teclado.nextLine();
-		String nomeFormatado = nomeDoFilmeEscrito.replace(" ", "+");
+		mapa.put(1L, lucas);
 		
-		
-		
-		System.out.println("https://www.omdbapi.com/?t=" + nomeFormatado +"&apikey=d6ffc961");
+		Pessoa lucas2 = mapa.get(1L);
+		System.out.println(lucas2.getNome() + " " + lucas.getId());
 	}
 }
